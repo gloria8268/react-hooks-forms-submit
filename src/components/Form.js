0c442347e227aa7676178e5ebbe890024eaa2e40
gloria <gloria.gao115@gmail.com>
2023-08-09 00:01:20 +1000

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,8 +12,31 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  // no save data
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = {
+      firstName: firstName,
+      lastName: lastName,
+    };
+    props.sendFormDataSomewhere(formData);
+    setFirstName("");
+    setLastName("");
+  }
+
+  // save previous data:
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   const formData = { firstName: firstName, lastName: lastName };
+  //   const dataArray = [...submittedData, formData];
+  //   setSubmittedData(dataArray);
+  //   setFirstName("");
+  //   setLastName("");
+  // }
+
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
